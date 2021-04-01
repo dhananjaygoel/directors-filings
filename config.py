4904b85b71f90c1fbff691799107afc25b89a405
Tanuja <tanuja.jain01@gmail.com>
2021-04-01 18:28:53 +0530

    initial files

---
+++ config.py
@@ -0,0 +1,16 @@
+
+#database
+database_name = 'dhananjaygoel$alphaminr_2021'
+port = 3306
+host = 'dhananjaygoel.mysql.pythonanywhere-services.com'
+password = 'alphaminr_mysql'
+user='dhananjaygoel'
+S3_filing_url = 'https://mblazr-filings.s3.amazonaws.com/static/filings/'
+
+database_name = 'mblazr5'
+port = 3308
+host = 'localhost'
+password = ''
+user='root'
+
+
